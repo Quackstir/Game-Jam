33498896c50bd373efd6ffb82361699ddb2d9980
CarlosE1996 <83315443+CarlosE1996@users.noreply.github.com>
2025-01-25 13:38:52 -0800

    Player Jumping
    Basic jump ability reintroduced

--- Global Game Jam 2025/Project/project-bubble-ggj-2025/Scripts/Character/player_movement_normal.gd
+++ Global Game Jam 2025/Project/project-bubble-ggj-2025/Scripts/Character/player_movement_normal.gd
@@ -1,27 +1,29 @@
 const SPEED = 150.0
 const ACCELERATION = 1000.0
+const JUMP_VELOCITY = -300.0
 const FRICTION = 25.0
-const GRAVITY_SCALE = 0.02
+const GRAVITY_SCALE = 0.2
 static var last_direction = Vector2.ZERO
-const PASSIVE_SPEED = 20.0
 
 func handle_input(player: CharacterBody2D, delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
-	direction = direction.normalized()
+	var input_axis := Input.get_axis("ui_left", "ui_right")
 	
-	if direction.x:
-		player.velocity.x = move_toward(player.velocity.x, direction.x * SPEED, ACCELERATION * delta)
-		last_direction.x = direction.x
+	if input_axis:
+		player.velocity.x = move_toward(player.velocity.x, input_axis * SPEED, ACCELERATION * delta)
 	else:
 		player.velocity.x = move_toward(player.velocity.x, 0, FRICTION * delta)
 	
-	# bubble gradually reaches passive gravity
-	if direction.y:
-		player.velocity.y = move_toward(player.velocity.y, direction.y * SPEED, ACCELERATION * delta)
-		last_direction.y = direction.y
-	else:
-		player.velocity.y = move_toward(player.velocity.y, player.get_gravity().y * GRAVITY_SCALE, FRICTION * delta)
+	apply_gravity(player,delta)
+	handle_jump(player)
 	
 	player.move_and_slide()
+
+func apply_gravity(player: CharacterBody2D, delta: float):
+	if not player.is_on_floor():
+		player.velocity += player.get_gravity() * GRAVITY_SCALE * delta
+
+func handle_jump(player: CharacterBody2D):
+	if Input.is_action_just_pressed("ui_accept"):
+		player.velocity.y = JUMP_VELOCITY
