09ac85dffaae8469423ee908dd2134535f7ed82c
Quackstir <51137567+Quackstir@users.noreply.github.com>
2025-01-25 15:29:15 -0800

    Made animations for Bubble Indicators
    -Added bubble animation when popping
    -Added bubble animation when reappearing
    -Made a setter function in currentJumps for bubble character to play certain animations on the bubble indicators depending on how many jumps the player has.

--- Global Game Jam 2025/Project/project-bubble-ggj-2025/Scripts/UI/HUD/DiageticHUD_BubbleManager.gd
+++ Global Game Jam 2025/Project/project-bubble-ggj-2025/Scripts/UI/HUD/DiageticHUD_BubbleManager.gd
@@ -3,11 +3,34 @@ extends Node2D
 
 const PREFAB_DIAGETIC_HUD_BUBBLE_JUMP_INDICATOR = preload("res://Objects/HUD/Prefab_DiageticHUD_BubbleJumpIndicator.tscn")
 
-@export var Player:CharacterBody2D
+@export var Player:bubble_character
 
 var BubbleIndicator_1:DiageticHUD_BubbleIndicator
 var BubbleIndicator_2:DiageticHUD_BubbleIndicator
 var BubbleIndicator_3:DiageticHUD_BubbleIndicator
+
+const BubbleOffset:int = 125
+const BubbleSeperation:int = 75
+
+var currentJumps = 3:
+	set(newValue):
+		match newValue:
+			0:
+				BubbleIndicator_1.isUsed = true
+				BubbleIndicator_2.isUsed = true
+				BubbleIndicator_3.isUsed = true
+			1:
+				BubbleIndicator_1.isUsed = false
+				BubbleIndicator_2.isUsed = true
+				BubbleIndicator_3.isUsed = true
+			2:
+				BubbleIndicator_1.isUsed = false
+				BubbleIndicator_2.isUsed = false
+				BubbleIndicator_3.isUsed = true
+			3:
+				BubbleIndicator_1.isUsed = false
+				BubbleIndicator_2.isUsed = false
+				BubbleIndicator_3.isUsed = false
 
 func _ready() -> void:
 	BubbleIndicator_1 = PREFAB_DIAGETIC_HUD_BUBBLE_JUMP_INDICATOR.instantiate()
@@ -21,3 +44,15 @@ func _ready() -> void:
 	BubbleIndicator_3 = PREFAB_DIAGETIC_HUD_BUBBLE_JUMP_INDICATOR.instantiate()
 	Player.add_child(BubbleIndicator_3)
 	BubbleIndicator_3.PositionMoveTowards = Vector2(-275,0)
+	
+	await get_tree().create_timer(3).timeout
+	currentJumps = 2
+
+	await get_tree().create_timer(1).timeout
+	currentJumps = 1
+	
+	await get_tree().create_timer(1).timeout
+	currentJumps = 0
+	
+	await get_tree().create_timer(1).timeout
+	currentJumps = 3
