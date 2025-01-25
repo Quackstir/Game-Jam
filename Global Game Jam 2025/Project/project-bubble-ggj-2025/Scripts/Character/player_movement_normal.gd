84fa291810db8fa3c646c5cf5abc20aba477676a
CarlosE1996 <83315443+CarlosE1996@users.noreply.github.com>
2025-01-25 14:39:38 -0800

    Player Jump Cooldown and Limit
    Added wip signal player_jumped that passes in jumps_left for player
    Added 3 jump limit
    Added cooldown between jumps
    No way to restore jumps left atm

--- Global Game Jam 2025/Project/project-bubble-ggj-2025/Scripts/Character/player_movement_normal.gd
+++ Global Game Jam 2025/Project/project-bubble-ggj-2025/Scripts/Character/player_movement_normal.gd
@@ -1,29 +1,44 @@
 const SPEED = 150.0
 const ACCELERATION = 1000.0
+const FRICTION = 200.0
+
+# JUMPING VALUES
 const JUMP_VELOCITY = -300.0
-const FRICTION = 25.0
 const GRAVITY_SCALE = 0.2
-static var last_direction = Vector2.ZERO
+const MAX_JUMPS = 3
+var _avaliable_jumps = 3
+const JUMP_COOLDOWN = 1.0
+var _jump_timer = 0.0
+
+signal player_jumped(jumps_left: int)
 
 func handle_input(player: CharacterBody2D, delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_axis := Input.get_axis("ui_left", "ui_right")
+	_apply_gravity(player,delta)
+	_handle_jump(player, delta)
+	_handle_horizontal_input(player,delta)
+	player.move_and_slide()
+
+func _apply_gravity(player: CharacterBody2D, delta: float):
+	if not player.is_on_floor():
+		player.velocity += player.get_gravity() * GRAVITY_SCALE * delta
+
+func _handle_jump(player: CharacterBody2D, delta: float):
+	#update jump timer
+	if _jump_timer > 0.0:
+		_jump_timer -= delta
+		if _jump_timer < 0.0:
+			_jump_timer = 0.0
 	
+	# As good practice, you should replace UI actions with custom gameplay actions.	
+	if Input.is_action_just_pressed("ui_accept") and _jump_timer == 0.0 and _avaliable_jumps != 0:
+		player.velocity.y = JUMP_VELOCITY
+		_jump_timer = JUMP_COOLDOWN
+		_avaliable_jumps -= 1
+		player_jumped.emit(_avaliable_jumps)
+
+func _handle_horizontal_input(player: CharacterBody2D, delta: float):
+	var input_axis := Input.get_axis("ui_left", "ui_right")
 	if input_axis:
 		player.velocity.x = move_toward(player.velocity.x, input_axis * SPEED, ACCELERATION * delta)
 	else:
 		player.velocity.x = move_toward(player.velocity.x, 0, FRICTION * delta)
-	
-	apply_gravity(player,delta)
-	handle_jump(player)
-	
-	player.move_and_slide()
-
-func apply_gravity(player: CharacterBody2D, delta: float):
-	if not player.is_on_floor():
-		player.velocity += player.get_gravity() * GRAVITY_SCALE * delta
-
-func handle_jump(player: CharacterBody2D):
-	if Input.is_action_just_pressed("ui_accept"):
-		player.velocity.y = JUMP_VELOCITY
