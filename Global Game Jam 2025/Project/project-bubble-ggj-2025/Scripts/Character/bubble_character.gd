2dc439b4a55d0a8b8ea78fcc5362834b7c9ae0dc
CarlosE1996 <83315443+CarlosE1996@users.noreply.github.com>
2025-01-25 10:19:19 -0800

    Character Movement Iteration
    Cleaned up movement code
    Re-introduced gravity
    Went for a very low friction

--- Global Game Jam 2025/Project/project-bubble-ggj-2025/Scripts/Character/bubble_character.gd
+++ Global Game Jam 2025/Project/project-bubble-ggj-2025/Scripts/Character/bubble_character.gd
@@ -1,20 +1,13 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
-const ACCELERATION = 1200.0
-const FRICTION = 200.0
-const STOPPING_FRICTION = 8.0
+const ACCELERATION = 1000.0
+const FRICTION = 25.0
 const GRAVITY_SCALE = 0.02
 static var last_direction = Vector2.ZERO
 const PASSIVE_SPEED = 20.0
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * GRAVITY_SCALE * delta
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
@@ -24,22 +17,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION * delta)
 		last_direction.x = direction.x
 	else:
-		if abs(velocity.x) < PASSIVE_SPEED + 1:
-			velocity.x = move_toward(velocity.x, 0, STOPPING_FRICTION * delta)
-		else:
-			velocity.x = move_toward(velocity.x, last_direction.x * PASSIVE_SPEED, FRICTION * delta)
-		
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 	
 	# bubble gradually reaches passive gravity
 	if direction.y:
 		velocity.y = move_toward(velocity.y, direction.y * SPEED, ACCELERATION * delta)
 		last_direction.y = direction.y
 	else:
-		#velocity.y = move_toward(velocity.y, get_gravity().y * GRAVITY_SCALE, FRICTION * delta)
-		#velocity.y = move_toward(velocity.y, last_direction.y * PASSIVE_SPEED, FRICTION * delta)
-		if abs(velocity.y) < PASSIVE_SPEED + 1:
-			velocity.y = move_toward(velocity.y, 0, STOPPING_FRICTION * delta)
-		else:
-			velocity.y = move_toward(velocity.y, last_direction.y * PASSIVE_SPEED, FRICTION * delta)
+		velocity.y = move_toward(velocity.y, get_gravity().y * GRAVITY_SCALE, FRICTION * delta)
 	
 	move_and_slide()
