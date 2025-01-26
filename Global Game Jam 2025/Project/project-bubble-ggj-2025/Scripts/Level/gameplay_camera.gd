f6c310795d2daa7e771999ea71c529af9077f93a
CarlosE1996 <83315443+CarlosE1996@users.noreply.github.com>
2025-01-25 19:48:33 -0800

    Camera Zoom Out Readded
    Restored camera zooming out when player grows

--- Global Game Jam 2025/Project/project-bubble-ggj-2025/Scripts/Level/gameplay_camera.gd
+++ Global Game Jam 2025/Project/project-bubble-ggj-2025/Scripts/Level/gameplay_camera.gd
@@ -2,6 +2,10 @@ extends Camera2D
 @export var camera_path : Array[camera_dock]
 
 @export var TRANSITION_SPEED = 0.6
+
+@export var ZOOM_SPEED = 0.5
+@export var ZOOM_SCALE_CHANGE = 0.8
+
 var _current_dock = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -19,3 +23,8 @@ func _on_bubble_character_player_reached_screen_edge() -> void:
 	if camera_path[_current_dock]:
 		var tween = create_tween()
 		tween.tween_property($".", "position", camera_path[_current_dock].position, TRANSITION_SPEED)
+
+
+func _on_bubble_character_player_grew(_growth_state: int) -> void:
+	var tween = create_tween()
+	tween.tween_property($".", "zoom", zoom * ZOOM_SCALE_CHANGE, ZOOM_SPEED)
