345e57f2bf3bccb861865a757311b80f6f980ddf
CarlosE1996 <83315443+CarlosE1996@users.noreply.github.com>
2026-01-31 13:26:50 -0800

    Guard slow turn on patrol

--- Global Game Jam 2026/GlobalGameJam2026/Common/SceneManager/scene_manager.gd
+++ Global Game Jam 2026/GlobalGameJam2026/Common/SceneManager/scene_manager.gd
@@ -1,11 +1,11 @@
 class_name SceneManager
 extends Node
 
-@export var level: Node2D
-@export var hud: Control
+@export var levels: Node2D
+@export var huds: Control
 
-var current_level
-var current_hud
+var _current_level
+var _current_hud
 
 func _ready():
 	Global.scene_manager = self
@@ -15,37 +15,40 @@ func change_level_scene(new_level: String, delete: bool = true,
 		keep_running: bool = false):
 	
 	# Deal with the current level
-	if current_level:
+	if _current_level:
 		if delete:
-			current_level.queue_free() # Remove level entirely
+			_current_level.queue_free() # Remove level entirely
 		elif keep_running:
-			current_level.visible = false # Keep level running in background
+			_current_level.visible = false # Keep level running in background
 		else:
-			level.remove_child(current_level) # Only keeps level in memory
+			levels.remove_child(_current_level) # Only keeps level in memory
 	
 	# switch to the new level
 	var new = load(new_level).instantiate()
-	level.add_child(new)
-	current_level = new
+	levels.add_child(new)
+	_current_level = new
 
 func change_hud_scene(new_hud: String, delete: bool = true,
 		keep_running: bool = false):
 	
-	# Deal with the current level
-	if current_hud:
+	# Deal with the current hud
+	if _current_hud:
 		if delete:
-			current_hud.queue_free()
+			_current_hud.queue_free()
 		elif keep_running:
-			current_hud.visible = false # Keep level running in background
+			_current_hud.visible = false # Keep hud running in background
 		else:
-			hud.remove_child(current_hud) # Only keeps level in memory
+			huds.remove_child(_current_hud) # Only keeps hud in memory
 	
-	# switch to the new level
+	# switch to the new hud
 	var new = load(new_hud).instantiate()
-	hud.add_child(new)
-	current_hud = new
+	huds.add_child(new)
+	_current_hud = new
+
+func get_current_level() -> Node2D:
+	return levels.get_child(0)
 
 func _boot_up_splash_screen():
 	var splash = load("res://Levels/CarlosTest/CarlosTestMenu.tscn").instantiate()
-	level.add_child(splash)
-	current_level = splash
+	levels.add_child(splash)
+	_current_level = splash
--- Global Game Jam 2026/GlobalGameJam2026/NPCs/Enemy/Guards/basic_guard.gd
+++ Global Game Jam 2026/GlobalGameJam2026/NPCs/Enemy/Guards/basic_guard.gd
@@ -4,32 +4,106 @@ extends BasicEnemy
 enum State{
 	IDLE,
 	PATROL,
+	PATROL_LOOK,
+	PATROL_TURN,
 	CHASE,
 	SEARCH,
 	RESET,
 }
 
+enum Dir{
+	NONE = -1,
+	UP,
+	LEFT,
+	RIGHT,
+	DOWN,
+}
+
 const PATROL_SPEED = 150
 const CHASE_SPEED = 250
+const TURN_SPEED = 5.0
 
+@export var current_dir: Dir = Dir.NONE
 @export var patrol_path: Path2D
 @export var path_follow: PathFollow2D
 
 var _current_state: State
-var _player_location: Vector2
-var _patrol_points: PackedVector2Array
-var _current_speed
+var _player_location: Vector2 # Last known player location
+var _waypoint: Area2D
+var _next_waypoint_index: int
+var _current_speed: int
+
+# local static variables
+var _turn_progress: float
+var _old_rotation: float
+var _goal_rotation: float
+var _turn_values_set: bool
 
 
 func _ready():
 	_current_state = State.PATROL
 	_player_location = Vector2.ZERO
-	if patrol_path:
-		_patrol_points = patrol_path.curve.get_baked_points()
+	_init_waypoint()
 	_current_speed = PATROL_SPEED
+	_turn_values_set = false
 
 
 func _process(delta):
+	_parent_waypoint_once()
 	match _current_state:
 		State.PATROL:
-			path_follow.progress += _current_speed * delta
+			_patrol(delta)
+		State.PATROL_TURN:
+			_turn_on_patrol(delta)
+
+
+func _init_waypoint():
+	_waypoint = find_child("WaypointArea")
+	_next_waypoint_index = 1
+	_waypoint.position = patrol_path.curve.get_point_position(_next_waypoint_index)
+	self.remove_child(_waypoint)
+
+
+func _parent_waypoint_once():
+	if not _waypoint.get_parent():
+		Global.scene_manager.get_current_level().add_child(_waypoint)
+
+
+func _patrol(delta):
+	path_follow.progress += _current_speed * delta
+
+func _turn_on_patrol(delta):
+	# set static local variables 
+	if not _turn_values_set:
+		_turn_progress = 0.0
+		_old_rotation = global_rotation
+		_goal_rotation = get_angle_to(_waypoint.global_position) + global_rotation
+		_turn_values_set = true
+	
+	# lerp rotation
+	global_rotation = lerp_angle(_old_rotation, _goal_rotation, _turn_progress)
+	_turn_progress += TURN_SPEED * delta
+	_turn_progress = clampf(_turn_progress, 0.0, 1.0)
+	
+	# reset values when completed
+	if is_equal_approx(fposmod(global_rotation, TAU), fposmod(_goal_rotation, TAU)):
+		_turn_values_set = false
+		_change_state(State.PATROL)
+
+func _change_state(new_state):
+	_current_state = new_state
+
+
+# called when patrol checkpoint reached
+func _on_waypoint_finder_area_entered(area):
+	if (area != _waypoint):
+		pass
+	_move_waypoint()
+	_change_state(State.PATROL_TURN)
+
+
+func _move_waypoint():
+	_next_waypoint_index += 1
+	if _next_waypoint_index >= patrol_path.curve.point_count:
+		_next_waypoint_index = 1
+	_waypoint.position = patrol_path.curve.get_point_position(_next_waypoint_index)
