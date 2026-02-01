0543218eacf5e4432d856ebda041f40c8080034b
Quackstir <51137567+Quackstir@users.noreply.github.com>
2026-02-01 11:17:32 -0800

    Making the win screen works
    -Made the buttons function in the win screen
    -Added camera to player

--- Global Game Jam 2026/GlobalGameJam2026/Levels/MarksTest/win_scene.gd
+++ Global Game Jam 2026/GlobalGameJam2026/Levels/MarksTest/win_scene.gd
@@ -4,3 +4,18 @@ extends Control
 
 func _ready() -> void:
 	label_2.text = "Score: " + str(GM.score)
+	get_tree().paused = true
+
+
+func _on_button_pressed() -> void:
+	get_tree().paused = false
+	GM.can_win = false
+	Global.scene_manager.huds.remove_child(self)
+	Global.scene_manager.reload_level()
+
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().paused = false
+	GM.can_win = false
+	Global.scene_manager.get_current_level().queue_free()
+	Global.scene_manager.change_hud_scene("res://Levels/MainMenu/main_menu.tscn")
